52988b9399e273d7a805ec20a5d8b83717a860e5
mikhail.safonov <mikhail.safonov@simbirsoft.com>
2017-08-29 17:54:22 +0300

    Добавил проект с unit тестами. Сделал тест на равенство полей модели и прокси-модели(модель Post).

---
+++ UnitTestProject/PostUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Models;
+using Core;
+using System.Linq;
+using Services.ProxyModels;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PostUnitTest
+    {
+        [TestMethod]
+        public void CompareDbEntityWithProxy()
+        {
+            Post post;
+            using (LearnDBContext context = new LearnDBContext())
+            {
+                post = context.Posts.First();
+            }
+
+            PostGetProxy postProxy = Services.Converters.Converter<Post, PostGetProxy>.Convert(post);
+
+            Assert.AreEqual(post.AuthorId, postProxy.AuthorId);
+            Assert.AreEqual(post.Description, postProxy.Description);
+            Assert.AreEqual(post.PostId, postProxy.PostId);
+            Assert.AreEqual(post.PublishDate, postProxy.PublishDate);
+            Assert.AreEqual(post.Text, postProxy.Text);
+        }
+    }
+}
